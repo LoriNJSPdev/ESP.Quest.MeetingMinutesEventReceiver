5b2393cb38ef2bfa70b2d71a97685e7f6ae110fd
LoriNJSPdev <lprzywozny@hotmail.com>
2017-10-04 20:28:28 +0000

    new repo
    Move to new repo

---
+++ Backup/ESP.Quest.MeetingMinutesEventReceiver/Features/MMReceiver/MMReceiver.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="febdd7e3-7870-41b0-8e13-ae0f67e2d8f0" featureId="febdd7e3-7870-41b0-8e13-ae0f67e2d8f0" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="ESP.Quest.MMReceiver" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="35ea1c90-f511-4b9e-9d90-b73e964cf6d7" />
+  </projectItems>
+</feature>
---
+++ Backup/ESP.Quest.MeetingMinutesEventReceiver/MeetingMinutesEventReceiver/ContextHelper.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Data.Objects;
+using System.Web;
+public static class ContextHelper<T> where T : ObjectContext, new()
+{   
+    
+    #region Methods
+    public static T GetCurrentContext(string ObjectContextKey) 
+    { 
+        HttpContext httpContext = HttpContext.Current;
+        if (httpContext != null)
+        { 
+            string contextTypeKey = ObjectContextKey + typeof(T).Name;
+            if (httpContext.Items[contextTypeKey] == null) 
+            { 
+                httpContext.Items.Add(contextTypeKey, new T());
+            } 
+            return httpContext.Items[contextTypeKey] as T; 
+        }    
+        throw new ApplicationException("There is no Http Context available");
+    }  
+    #endregion
+}
